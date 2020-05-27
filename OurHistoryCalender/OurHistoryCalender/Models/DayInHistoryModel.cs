82326c2159cb94712c3c779b47c3e9844bbb39c4
cbillin7 <yungregg1e@yahoo.com>
2020-05-26 21:13:15 -0400

    Added model for date layout updated language for project

---
+++ OurHistoryCalender/OurHistoryCalender/Models/DayInHistoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurHistoryCalender.Models
+{
+    class DayInHistoryModel
+    {
+        public DateTime DateInHistory { get; set; }
+        public string BriefDescription { get; set; }
+        public string Image { get; set; }
+        public string Source { get; set; }
+    }
+}
