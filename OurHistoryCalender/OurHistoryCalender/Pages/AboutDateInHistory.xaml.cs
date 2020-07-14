1320e16457c67093d219bc73aa1ddd3275e48eb6
cbillin7 <yungregg1e@yahoo.com>
2020-07-14 06:18:17 -0400

    added new page to view this particular day in history

---
+++ OurHistoryCalender/OurHistoryCalender/Pages/AboutDateInHistory.xaml.cs
@@ -0,0 +1,26 @@
+﻿using OurHistoryCalender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OurHistoryCalender.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutDateInHistory : ContentPage
+    {
+        public AboutDateInHistory()
+        {
+            InitializeComponent();
+        }
+
+        public string GetHistory(DayInHistory selectedDayInHistory)
+        {
+            return selectedDayInHistory.BriefDescription;
+        }
+    }
+}
