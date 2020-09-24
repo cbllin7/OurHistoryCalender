e87cbc422b1bf27ea250773427166c62941b63da
cbillin7 <yungregg1e@yahoo.com>
2020-09-24 19:19:41 -0400

    Updated add special day

--- OurHistoryCalender/OurHistoryCalender/Pages/AddSpecialDay.xaml.cs
+++ OurHistoryCalender/OurHistoryCalender/Pages/AddSpecialDay.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OurHistoryCalender.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,11 @@ namespace OurHistoryCalender.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddSpecialDay : ContentPage
     {
+        public DayInHistory newDayInHistory;
         public AddSpecialDay()
         {
             InitializeComponent();
+            newDayInHistory = new DayInHistory();
         }
     }
 }
